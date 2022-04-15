dd2d3f567a0955b9823d676c70905557f8f4937c
Daniel Cruz <danielcunha54@gmail.com>
2022-04-14 21:42:12 -0300

    chore: setup debugger

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,6 @@ import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3333;
+app.use(express.json());
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
