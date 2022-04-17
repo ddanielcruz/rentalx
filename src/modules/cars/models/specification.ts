f167766e14e7e81f3451109ea25df0882d050c6f
Daniel Cruz <danielcunha54@gmail.com>
2022-04-17 08:39:07 -0300

    refactor: move resources to cars module

---
+++ src/modules/cars/models/specification.ts
@@ -0,0 +1,17 @@
+import { v4 as uuid } from "uuid";
+
+class Specification {
+  id: string;
+  name: string;
+  description: string;
+  createdAt: Date;
+
+  constructor(name: string, description: string) {
+    this.id = uuid();
+    this.name = name;
+    this.description = description;
+    this.createdAt = new Date();
+  }
+}
+
+export { Specification };
